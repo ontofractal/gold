a7715c8f8357e2f1687c5e60fc15ffaed7668fba
ontofractal <ontofractal@users.noreply.github.com>
2017-12-18 09:47:31 +0000

    Update gold.ex

--- lib/gold.ex
+++ lib/gold.ex
@@ -294,7 +294,7 @@ defmodule Gold do
 
     headers = ["Authorization": "Basic " <> Base.encode64(user <> ":" <> password)]
 
-    options = [timeout: 600000, recv_timeout: 600000]
+    options = [timeout: 1800000, recv_timeout: 1800000]
 
     case HTTPoison.post("http://" <> hostname <> ":" <> to_string(port) <> "/", Poison.encode!(command), headers, options) do
       {:ok, %{status_code: 200, body: body}} ->
